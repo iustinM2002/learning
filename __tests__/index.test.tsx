193d21c5002b20a0c8d8daa68c8b897401f111cb
iustinM2002 <iustin.morosanu@hartehanks.com>
2022-05-03 16:27:22 +0300

    2

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -12,7 +12,8 @@ jest.mock('api' ,() => ({
 describe('sign in', () => {
   it('renders a heading', async () => {
     const element:any = jest.fn()
-      render(<LobbyTable element={element}/>);
+    const element2:any = jest.fn()
+      render(<LobbyTable element={element} setActiveElement={element2}/>);
       const listItem = await waitFor(() => screen.getByTestId('title_table0'));
       await waitFor(()=>expect(listItem).toBeInTheDocument());
   })
